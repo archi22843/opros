cbc13028f547bc8179234a138eb41c046edb75d5
archi22843 <danzpono652@gmail.com>
2025-04-11 12:21:51 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+function t (){
+  alert("правельно");
+}
+function f () {
+  alert("неправельно");
+}
+
+function ft() {
+  var trv=confirm("ты уверен/уверена?");
+  if (trv==true) {
+    alert("неправельно")
+  }
+  else {
+    alert("правельно");
+  }
+}
